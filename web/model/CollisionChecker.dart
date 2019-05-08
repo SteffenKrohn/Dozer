6ec087e2a672c95f1fed2695fa874d370add3048
SteffenKrohn <jan.steffen.krohn@stud.fh-luebeck.de>
2019-05-08 22:11:54 +0200

    Implement CollisionChecker methods

--- web/model/CollisionChecker.dart
+++ web/model/CollisionChecker.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:math';
 
 import '../model/Element.dart' as m;
 
@@ -45,14 +46,55 @@ class CollisionChecker {
    * by checking the boundaries of both elements
    */
   static bool circles(m.Element a, m.Element b) {
-    //TODO
+    int x1, x2, y1, y2;
+    x1 = (a.x + (a.width / 2)) as int;
+    x2 = (b.x + (b.width / 2)) as int;
+    y1 = (a.y + (a.height / 2)) as int;
+    y2 = (b.y + (b.height / 2)) as int;
+
+    if (y1 > y2) {
+      int tmp = y1;
+      y1 = y2;
+      y2 = tmp;
+    }
+    if (x1 > x2) {
+      int tmp = x1;
+      x1 = x2;
+      x2 = tmp;
+    }
+
+    if (sqrt((x2 - x1)^2 + (y2 - y1)^2) <= (a.height / 2) + (b. height / 2)) {
+      return true;
+    }
+    return false;
   }
 
   /**
    * Checks if two elements with circle and rectangle shape overlap or touch
    * by checking the boundaries of both elements
    */
-  static bool recCir(m.Element rectanlge, m.Element circle) {
-    //TODO
+  static bool recCir(m.Element rectangle, m.Element circle) {
+
+    int x = (circle.x + (circle.width / 2)) as int;
+    int y = (circle.y + (circle.height / 2)) as int;
+
+
+    // Upper left
+    if (sqrt((rectangle.x - x)^2 + (rectangle.y - y)^2) <= (circle.height / 2)) {
+      return true;
+    }
+    // Upper right
+    if (sqrt((x - rectangle.x)^2 + (rectangle.y - y)^2) <= (circle.height / 2)) {
+      return true;
+    }
+    // Lower Left
+    if (sqrt((rectangle.x - x)^2 + (y - rectangle.y)^2) <= (circle.height / 2)) {
+      return true;
+    }
+    // Lower right
+    if (sqrt((x - rectangle.x)^2 + (y - rectangle.y)^2) <= (circle.height / 2)) {
+      return true;
+    }
+    return false;
   }
 }
